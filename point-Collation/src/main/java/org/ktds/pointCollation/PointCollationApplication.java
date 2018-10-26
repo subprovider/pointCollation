6012abab78723879e496de3cb6741d5754804c67
danielKim <subprovider@gmail.com>
2018-10-26 11:24:41 +0900

    pointCollation init

---
+++ point-Collation/src/main/java/org/ktds/pointCollation/PointCollationApplication.java
@@ -0,0 +1,12 @@
+package org.ktds.pointCollation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointCollationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointCollationApplication.class, args);
+	}
+}
